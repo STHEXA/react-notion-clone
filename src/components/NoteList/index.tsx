19ba2318f4914eab3704e0347a236ed6fdac51c6
STHEXA <ti.syu.s@gmail.com>
2025-04-12 10:04:45 +0900

    Supabaseから子供のノートを取得する処理作成

--- src/components/NoteList/index.tsx
+++ src/components/NoteList/index.tsx
@@ -3,6 +3,8 @@ import { NoteItem } from "./NoteItem";
 import { useNoteStore } from "@/modules/notes/note.state";
 import { useCurrentUserStore } from "@/modules/auth/current-user.state";
 import { noteRepository } from "@/modules/notes/note.repository";
+import { Note } from "@/modules/notes/note.entity";
+import React from "react";
 
 interface NoteListProps {
   layer?: number;
@@ -18,6 +20,13 @@ export function NoteList({ layer = 0, parentId }: NoteListProps) {
     e.stopPropagation();
     const newNote = await noteRepository.create(currentUser!.id, { parentId });
     noteStore.set([newNote]);
+  };
+
+  const fetchChildren = async (e: React.MouseEvent, note: Note) => {
+    e.stopPropagation();
+    const children = await noteRepository.find(currentUser!.id, note.id);
+    if (children === null) return;
+    noteStore.set(children);
   };
 
   return (
@@ -37,6 +46,7 @@ export function NoteList({ layer = 0, parentId }: NoteListProps) {
             <NoteItem
               note={note}
               layer={layer}
+              onExpand={(e: React.MouseEvent) => fetchChildren(e, note)}
               onCreate={(e) => createdChild(e, note.id)}
             />
           </div>
