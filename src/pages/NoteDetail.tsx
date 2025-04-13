df2b92f971b577efeaa980429a6645d41a26e646
STHEXA <ti.syu.s@gmail.com>
2025-04-13 23:08:57 +0900

    ノートのタイトルの更新処理を作成

--- src/pages/NoteDetail.tsx
+++ src/pages/NoteDetail.tsx
@@ -25,25 +25,27 @@ const NoteDetail = () => {
     setIsLoading(false);
   };
 
-  // const updateNote = async (
-  //   id: number,
-  //   note: { title?: string; context?: string }
-  // ) => {
-  //   const updatedNote = await noteRepository.update(id, note);
-  //   if (updatedNote == null) return;
-  //   noteStore.set([updatedNote]);
-  //   return updatedNote;
-  // };
+  const updateNote = async (
+    id: number,
+    note: { title?: string; context?: string }
+  ) => {
+    const updatedNote = await noteRepository.update(id, note);
+    if (updatedNote == null) return;
+    noteStore.set([updatedNote]);
+    return updatedNote;
+  };
 
   if (isLoading) return <div></div>;
   if (note == null) return <div>note is not existed</div>;
-  console.log("何度も呼ばれてる？");
   console.log(note);
 
   return (
     <div className="pb-40 pt-20">
       <div className="md:max-w-3xl lg:md-max-w-4xl mx-auto">
-        {/* <TitleInput /> */}
+        <TitleInput
+          initialData={note}
+          onTitleChange={(title) => updateNote(id, { title })}
+        />
       </div>
     </div>
   );
