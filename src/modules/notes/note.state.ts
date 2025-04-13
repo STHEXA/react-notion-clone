44d1703d88f533f7be28aec4c3492e540ae9f03a
STHEXA <ti.syu.s@gmail.com>
2025-04-13 21:22:40 +0900

    WIP

--- src/modules/notes/note.state.ts
+++ src/modules/notes/note.state.ts
@@ -17,8 +17,11 @@ export const useNoteStore = () => {
     });
   };
 
+  const getOne = (id: number) => notes.find((note) => note.id === id);
+
   return {
     getAll: () => notes,
+    getOne,
     set,
   };
 };
