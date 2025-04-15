179306e400e6e37a33f1734b901b4e2bbfea21c6
STHEXA <ti.syu.s@gmail.com>
2025-04-15 22:54:57 +0900

    use-debounceの導入

--- src/components/SearchModal.tsx
+++ src/components/SearchModal.tsx
@@ -8,6 +8,7 @@ import {
   CommandList,
 } from "@/components/ui/command";
 import { Note } from "@/modules/notes/note.entity";
+import { useDebouncedCallback } from "use-debounce";
 
 interface SearchModalProps {
   isOpen: boolean;
@@ -24,12 +25,13 @@ export function SearchModal({
   onKeywordChanged,
   onClose,
 }: SearchModalProps) {
+  const debounced = useDebouncedCallback(onKeywordChanged, 500);
   return (
     <CommandDialog open={isOpen} onOpenChange={onClose}>
       <Command shouldFilter={false}>
         <CommandInput
           placeholder={"キーワードで検索"}
-          onValueChange={onKeywordChanged}
+          onValueChange={debounced}
         />
         <CommandList>
           <CommandEmpty>条件に一致するノートがありません</CommandEmpty>
